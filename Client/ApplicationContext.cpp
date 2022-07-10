865c3750fb85b7ecaca453f6885657c3e0ae93f6
kimjunho403 <kimjunho403@gmail.com>
2022-07-10 14:42:55 +0900

    컨플릭스 수정줌

--- Client/ApplicationContext.cpp
+++ Client/ApplicationContext.cpp
@@ -912,7 +912,7 @@ void ApplicationContext::CreateMinimap()
 		icon->m_World._22 = 1;
 		icon->m_World._33 = 15;
 		icon->Scale(10, 10, 10);
-		XMStoreFloat4x4(&icon->m_World, XMLoadFloat4x4(&icon->m_World) * XMMatrixRotationX(-3.141592 * (90 - 0.4 * sqrt(5)) / 180.0f));
+		XMStoreFloat4x4(&icon->m_World, XMLoadFloat4x4(&icon->m_World) * XMMatrixRotationX(-3.141592 * (45 - 0.4 * sqrt(5)) / 180.0f));
 
 		icon->m_TexTransform = MathHelper::Identity4x4();
 	}
